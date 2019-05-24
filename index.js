0bad7aac8a10cf9dc89341319988c7cdcb97ee9d
Justin Gorham <justingorham@gmail.com>
2019-05-24 00:13:56 -0400

    another test

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log(process.env);
+const { exec } = require('child_process');
+
+const theThing = async () => {
+    const result = await exec.__promisify__('git branch')
+    console.log(result);
+}
+
+theThing();
