6478620073f0b4c3ea693e7f52f37958dfa103c2
Justin Gorham <justingorham@gmail.com>
2019-05-24 00:20:41 -0400

    tests

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const { exec } = require('child_process');
+const execp = (...args) => new Promise((res, rej) => {
+    exec(...args, (err, stdout, stderr) => {
+        return err ? rej(err) : res({ stdout, stderr });
+    })
+});
 
 const theThing = async () => {
-    const result = await exec.__promisify__('git branch')
+    const result = await execp('git branch')
     console.log(result);
 }
 
