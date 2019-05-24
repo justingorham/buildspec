edd8a12d6149bacd5ce253fef3e2d0bbf5a85181
Justin Gorham <justingorham@gmail.com>
2019-05-24 01:10:03 -0400

    more changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { exec } = require('child_process');
+const { EOL } = require('os')
 const execp = (...args) => new Promise((res, rej) => {
     exec(...args, (err, stdout, stderr) => {
         return err ? rej(err) : res({ stdout, stderr });
@@ -7,7 +8,8 @@ const execp = (...args) => new Promise((res, rej) => {
 
 const theThing = async () => {
     const { CODEBUILD_RESOLVED_SOURCE_VERSION } = process.env;
-    const result = await execp(`git branch -a --contains ${CODEBUILD_RESOLVED_SOURCE_VERSION}`)
+    const { stdout } = await execp(`git branch -a --contains ${CODEBUILD_RESOLVED_SOURCE_VERSION}`)
+    const lines = stdout.split(EOL);
     console.log(result);
 }
 
